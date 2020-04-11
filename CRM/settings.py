f086610b0402a16c03e44a2b2607072ef6324f79
Mario Pleitez <mariopleitez@gmail.com>
2020-04-11 10:09:42 -0600

    aws-rds

--- CRM/settings.py
+++ CRM/settings.py
@@ -75,12 +75,38 @@ WSGI_APPLICATION = 'CRM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+ #       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ #   }
+#} 
+
+
+#DATABASES = {
+    #'default': {
+       # 'ENGINE': 'django.db.backends.postgresql',
+      #  'NAME': 'democrm',
+     #   'USER': 'postgres',
+    #    'PASSWORD': 'mep1908c22',
+   #     'HOST': 'localhost',
+  #      'POST': '5432'
+ #   }
+#}
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbcrmdemo',
+        'USER': 'mariopleitez',
+        'PASSWORD': 'mep1908c22',
+        'HOST': 'database-1.cu0lniijbya0.us-east-1.rds.amazonaws.com',
+        'POST': '5432'
     }
 }
+
 
 
 # Password validation
